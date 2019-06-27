baf66ec771ff9c24c4797838c4411b50fe3c0cf6
Szücs József <szujoz95@gmail.com>
2019-06-28 00:46:15 +0200

    DHT adafruit library is encapsulated in myDHT/DHT_11 class. Process shows valid values.

--- SmartBoi_Slave1/myDht11.cpp
+++ SmartBoi_Slave1/myDht11.cpp
@@ -1,89 +1,88 @@
 #include "myDht11.h"
 
-DHT11::DHT11(int Pin)
+DHT_11* DHT_11::instance = NULL;
+
+DHT_11::DHT_11(int Pin)
 {
   pin          = Pin;
   temperature  = 0.0f;
   humidity     = 0.0f;
+  humiture     = 0.0f;
+
+  dht11 = new DHT(Pin, DHT11);
 }
 
-void DHT11::init(void)
+DHT_11* DHT_11::getInstance(const int Pin)
 {
-  pinMode(pin, OUTPUT);
-  // Bus down, send start signal.
-  digitalWrite(pin, LOW);
-  // Delay greater than 18ms, so DHT11 start signal can be detected.
-  delay(30); 
+  if (instance == NULL)
+  {
+    instance = new DHT_11(Pin);
+  }
+
+  return instance;
 }
 
-int DHT11::getPin(void)
+DHT_11::~DHT_11(void)
+{
+  free(dht11);
+  free(instance);
+}
+
+void DHT_11::init(void)
+{
+  dht11->begin();
+}
+
+int DHT_11::getPin(void)
 {
   return pin;
 }
 
-/// TODO debug
-void DHT11::process(void)
+float DHT_11::getTempr(void)
 {
-  uint8_t data[5];
-  
-  digitalWrite(pin, LOW); // bus down, send start signal
-  delay(30); // delay greater than 18ms, so DHT11 start signal can be detected
- 
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(40); // Wait for DHT11 response
- 
-  pinMode(pin, INPUT);
-  /// TODO timeout
-  /*for(int i = 0; i < 200; i++)
-  {
-    if (digitalRead(pin) == HIGH)   break;
-  }*/
-  while (digitalRead(pin) == HIGH);
-  delayMicroseconds(80); // DHT11 response, pulled the bus 80us
-  if (digitalRead(pin) == LOW);
-    delayMicroseconds(80); // DHT11 80us after the bus pulled to start sending data
-
-  /// TODO 5th is crc?
-  for (int i = 0; i < 4; i ++) // receive temperature and humidity data, the parity bit is not considered
-    data[i] = readData();
-
-  humidity    = ((float)data[0]) + ((float)((int)data[1])/100.0f);
-  temperature = ((float)data[2]) + ((float)((int)data[3])/100.0f);
-
-  Serial.print(data[0]);
-  Serial.print("  ");
-  Serial.print(data[1]);
-  Serial.print("  ");
-  Serial.print(data[2]);
-  Serial.print("  ");
-  Serial.print(data[3]);
-  Serial.print("  Derived: ");
-  Serial.print(temperature);
-  Serial.print(" ");
-  Serial.println(humidity);
- 
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH); // send data once after releasing the bus, wait for the host to open the next Start signal
+  return temperature;
 }
 
-///TODO debug
-uint8_t DHT11::readData(void)
+float DHT_11::getHumid(void)
 {
-  uint8_t data;
+  return humidity;
+}
 
-  for (int i = 0; i < 8; i ++)
-  {
-    if (digitalRead(pin) == LOW)
-    {
-      while (digitalRead(pin) == LOW);     // wait for 50us
-        delayMicroseconds(30);             // determine the duration of the high level to determine the data is '0 'or '1'
-      
-      if (digitalRead(pin) == HIGH)
-        data |= (1 << (7-i));               // high front and low in the post
+/// TODO debug
+void DHT_11::process(void)
+{
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht11->readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht11->readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht11->readTemperature(true);
 
-      while (digitalRead (pin) == HIGH);    // data '1 ', wait for the next one receiver
-     }
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
-  
-  return data;
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht11->computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht11->computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  temperature = t;
+  humidity = h;
+  humiture = hic;
 }
--- SmartBoi_Slave1/myDht11.h
+++ SmartBoi_Slave1/myDht11.h
@@ -1,20 +1,32 @@
 #include <Arduino.h>
 
-///KY015 DHT11 Temperature and humidity sensor 
-class DHT11
+///KY015 DHT11 Temperature and humidity sensor
+#include <DHT.h>
+#include <DHT_U.h>
+
+class DHT_11
 {
   private:
+      static DHT_11* instance;
+
+      DHT*  dht11;
       int   pin;
       float temperature;
       float humidity;
+      float humiture;
 
-      uint8_t readData(void);
+      DHT_11(const int Pin);
 
   public:
-      DHT11(int Pin);
-      void init(void);
+      static DHT_11* getInstance(const int Pin);
+
+      ~DHT_11(void);
       
+      void init(void);
+
       int getPin(void);
+      float getTempr(void);
+      float getHumid(void);
 
       void process(void);
 };
